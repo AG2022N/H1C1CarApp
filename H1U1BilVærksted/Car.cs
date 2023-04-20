3bd7ff61a57704ae74b69d21f0ec0bf2b148c20d
Allan <AGawron2021@protonmail.com>
2023-04-20 10:56:01 +0200

    Initial push

---
+++ H1U1BilVærksted/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1U1BilVærksted
+{
+    internal class Car
+    {
+        string CarBrand, CarModel;
+        //ManufactureYear, LastCheckedDate
+    }
+}
---
+++ H1U1BilVærksted/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Indtast Mærke");
+    Console.WriteLine("Indtast Model");
+    Console.WriteLine("Indtast Produktionsdato");
+    Console.WriteLine("Indtast Dato for sidste syn");
+
+
+
+
+    Console.ReadKey();
+}
+
+
+//Car Car1 = new Car();
+
+
+
+//bool x = false;
+//if (x == true)
+//{
+//    //Conclusion = "Bilen skal til syn"
+//    Console.WriteLine("Bilen skal til syn");
+//}
+//else if (x == false)
+//{
+//    //Conclusion = "Bilen skal ikke synes"
+//    Console.WriteLine("Bilen skal ikke synes");
+//}
+
+
+
+
+//Example for Output:
+//==============================================
+//Tjekkede: 2023.04.19
+//Mærke: Audi
+//Model: E
+//Produktionsdato: 2020
+//Sidst synes: N/A
+//Konklusion: Bilen skal ikke synes
+//
+//==============================================
+//Tjekkede: 2023.04.19
+//Mærke: Fiat
+//Model: N
+//Produktionsdato: 2018
+//Sidst synes: N/A
+//Konklusion: Bilen skal til syn
+//
